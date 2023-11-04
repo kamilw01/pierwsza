caeb5012b3be3289eb47fc822a33b650f586dba6
kamilw01 <147665208+kamilw01@users.noreply.github.com>
2023-11-04 23:12:02 +0100

    zmiana 3

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,18 +6,24 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            byte number = 2;
-            int count = 10;
-            float totalPrice = 20.95f;
-            char character = 'A';
-            string firstName = "Kamil";
-            bool isWorking = true;
+            var number = 2;
+            var count = 10;
+            var totalPrice = 20.95f;
+            var character = 'A';
+            var firstName = "Kamil";
+            var isWorking = true;
             Console.WriteLine(number);
             Console.WriteLine(count);
             Console.WriteLine(totalPrice);
             Console.WriteLine(character);
             Console.WriteLine(firstName);
             Console.WriteLine(isWorking);
+
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+
+            const float Pi = 3.14f;
+            // int nie może przekompilować się w byte!
         }
     }
 }
